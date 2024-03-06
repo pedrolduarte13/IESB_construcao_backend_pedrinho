e39e9caac430c8a207d8e1dfa6f39f82d9863310
pedrolduarte13 <pedro.l.duarte@iesb.edu.br>
2024-03-06 19:51:17 -0300

    teste 2

---
+++ fundamentos/fundamentos_aula03.js
@@ -0,0 +1,34 @@
+// Imprimindo informações no terminal
+console.log("TESTE");
+
+// Variaveis
+// var, const, let
+// var (Comando antigo)
+var testandoVar = "TestanoVar"
+console.log(testandoVar);
+
+// Const >> Criar uma constante (variavel que não sofre alteração)
+const PI = 3,14
+
+// PI = 9999
+
+console.log(PI);
+
+// Let >> Criar uma variavel que pode sofrer alterações
+let nomeCompleto
+console.log(nomeCompleto);
+nomeCompleto = "Pedro"
+console.log(nomeCompleto);
+nomeCompleto = "Pedro Lucas"
+console.log(nomeCompleto)
+
+// Tipos de dados
+String
+Number
+Boolean
+Object
+
+null -> 
+undefined -> Ausencia de valor
+NaN
+
